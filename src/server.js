96c81b8a7a45d7cdb41849871ae7378db863e763
guillermoFragachan <guillefragachan@gmail.com>
2021-10-29 14:15:30 +0200

    update server

--- src/server.js
+++ src/server.js
@@ -8,18 +8,28 @@ const server = express()
 
 
 
+const whitelist = [process.env.HOST, process.env.FE_LOCAL_URL, process.env.FE_PUBLIC_URL]
+const corsOpts = {
+  origin: function (origin, next) {
+    console.log("CURRENT ORIGIN: ", origin)
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      next(null, true)
+    } else {
+      next(new Error("CORS ERROR"))
+    }
+  },
+}
 
 
 
-
-server.use(cors())
+server.use(cors(corsOpts))
 // server.use(express.static(publicFolderPath))
 
 server.use(express.json())
 
 server.use('/media', mediaRouter)
 
-const port = 3001
+const port = process.env.PORT
 
 
 
